2edf92744624bd141e39155216859058b51520aa
Shaun MR <shaunmr009@gmail.com>
2024-08-30 08:06:19 +0530

    final

---
+++ questions.js
@@ -0,0 +1,98 @@
+let questions = [
+    {
+        numb :1,
+        question : " What does the <a> tag in HTML stand for?",
+        answer:"b. Anchor",
+        options:[
+           `a. Attribute`,
+           `b. Anchor`,
+           `c. Array`,
+           `d. Aspect`
+        ]
+
+    },
+    {
+        numb :2,
+        question : "Which property is used to change the background color of an element in CSS?",
+        answer:"b. background-color",
+        options:[
+            `a. color`,
+            `b. background-color`,
+            `c. bgcolor`,
+            `d. back-color`
+        ]
+
+    },
+    {
+        numb :3,
+        question : "Which method is used to write content to an HTML document using JavaScript?",
+        answer:"a. document.write()",
+        options:[
+            `a. document.write()`,
+            `b. document.create()`,
+            `c. document.content()`,
+            `d. document.print()`
+        ]
+
+    },
+    {
+        numb :4,
+        question : "Which HTML element is used for the largest heading?",
+        answer:"a. h1",
+        options:[
+            `a. h1`,
+            `b. h2`,
+            `c. h3`,
+            `d. h4`
+        ]
+
+    },
+    {
+        numb :5,
+        question : " How do you select an element with the class name example in CSS?",
+        answer:"a. .example",
+        options:[
+            `a. .example`,
+            `b. #example`,
+            `c. example`,
+            `d. *example`
+        ]
+
+    }/*,
+    {
+        numb :6,
+        question : " How do you select an element with the class name example in CSS?",
+        answer:"a. .example",
+        options:[
+            `a. .example`,
+            `b. #example`,
+            `c. example`,
+            `d. *example`
+        ]
+
+    },
+    {
+        numb :7,
+        question : " How do you select an element with the class name example in CSS?",
+        answer:"a. .example",
+        options:[
+            `a. .example`,
+            `b. #example`,
+            `c. example`,
+            `d. *example`
+        ]
+
+    },
+    {
+        numb :8,
+        question : " How do you select an element with the class name example in CSS?",
+        answer:"a. .example",
+        options:[
+            `a. .example`,
+            `b. #example`,
+            `c. example`,
+            `d. *example`
+        ]
+
+    }*/
+];
